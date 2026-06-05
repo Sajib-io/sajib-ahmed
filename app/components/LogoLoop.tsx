adc73a9f2fd5aab3ed34320b679e8e2a54d0d682
sajibahmed0011 <mdsajib0162529@gmail.com>
2026-06-05 22:06:36 +0600

    fix: move LogoLoop CSS into globals to fix TS build error

--- app/components/LogoLoop.tsx
+++ app/components/LogoLoop.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useCallback, useEffect, useMemo, useRef, useState, memo } from "react";
-import "./LogoLoop.css";
 
 const ANIMATION_CONFIG = { SMOOTH_TAU: 0.25, MIN_COPIES: 2, COPY_HEADROOM: 2 };
 
